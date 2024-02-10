3e9b55c4e59c1da637c193de70b95bc0af91b39d
Bowei Kou <kou.76@osu.edu>
2024-02-10 16:24:57 -0500

    update

--- Interface/IController.cs
+++
@@ -1,7 +0,0 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-public interface IController
-{
-	void Update(GameTime gameTime);
-}
--- Interface/IProjectiles.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-
-public interface IProjectiles
-{
-	void Update(GameTime gameTime);
-	void Draw(SpriteBatch spirtBatch);
-}
