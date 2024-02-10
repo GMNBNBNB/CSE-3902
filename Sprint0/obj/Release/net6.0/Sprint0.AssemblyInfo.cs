2123bec0bd61f883877d2c8fafdb7b15cb61d748
GM <149216583+GMNBNBNB@users.noreply.github.com>
2024-02-09 21:48:08 -0500

    Delete .vs directory

--- Sprint0/obj/Release/net6.0/Sprint0.AssemblyInfo.cs
+++ Sprint0/obj/Release/net6.0/Sprint0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sprint0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31c49412e51ae66b6681a6f4ff8b879434c9d957")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sprint0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sprint0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
