b5917304fbe638efad4370adee6d99e634fa84a3
GM <149216583+GMNBNBNB@users.noreply.github.com>
2024-02-23 19:26:04 -0500

    Delete Sprint0 directory

--- Sprint0/Interface/ICommand.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-
-public interface ICommand
-{
-    void Execute();
-}
--- Sprint0/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿
-using var game = new Sprint0.Game1();
-game.Run();
